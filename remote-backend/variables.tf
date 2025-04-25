fc31d2d97df8ea5162db3c8058c7108b5ebd483a
akalanka <akalankapathirage22@gmail.com>
2025-04-25 10:08:18 +0530

    Update default values in variables.tf to use placeholders for project configuration

--- remote-backend/variables.tf
+++ remote-backend/variables.tf
@@ -1,19 +1,19 @@
 variable "project_name" {
   description = "The name of the project."
   type        = string
-  default     = "smata-pipeline0" # REPLACE WITH YOUR PROJECT NAME
+  default     = "PROJECT_NAME" # REPLACE WITH YOUR PROJECT NAME
 }
 
 variable "bucket_name" {
   description = "The name of the S3 bucket to store the Terraform state file."
   type        = string
-  default     = "smata-pipeline0-tf-state" # REPLACE WITH YOUR BUCKET NAME
+  default     = "PROJECT_NAME-tf-state" # REPLACE WITH YOUR BUCKET NAME
 }
 
 variable "table_name" {
   description = "The name of the DynamoDB table for state locking."
   type        = string
-  default     = "smata-pipeline0-tf-state-locking" # REPLACE WITH YOUR TABLE NAME
+  default     = "PROJECT_NAME-tf-state-locking" # REPLACE WITH YOUR TABLE NAME
 }
 
 variable "aws_region" {
